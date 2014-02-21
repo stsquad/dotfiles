14be43cd7f3dbd353227114f15f00bc02ccad2ea
Alex Bennée <alex.bennee@linaro.org>
2014-02-21 11:29:29 +0000

    dotbach_emacs/i3: fix up i3lock handling with xcape
    See http://superuser.com/questions/679284/how-do-i-get-a-valid-xmodmap-while-using-xcape

--- dotconfig/i3/lock_screen.sh
+++ dotconfig/i3/lock_screen.sh
@@ -5,4 +5,10 @@
 # As I have to mess around with xmodmaps and the like I route all screen locking through here
 #
 
+# The initial key-sequence chosen by xcape does confuse i3lock so we reset it
+xmodmap -e "keycode 8 = "
+
+# Ensure the return key does work as intended
+xmodmap -e 'keycode 36 = Return'
+
 i3lock -c 334433
