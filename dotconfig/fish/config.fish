670a78db40e7b82dd01b738e71d5a443397222b7
Alex Bennée <alex.bennee@linaro.org>
2017-01-31 15:18:28 +0000

    fish.config: add copy_to_nas helper

--- dotconfig/fish/config.fish
+++ dotconfig/fish/config.fish
@@ -7,3 +7,9 @@ set -gx HOSTNAME (hostname)
 if status --is-interactive;
         keychain --eval --quiet -Q id_rsa
 end
+
+function copy_to_nas -d "Copy files to the NAS"
+    for f in "$argv";
+        scp "$f" admin@nas:/volume1/video/
+    end
+end
