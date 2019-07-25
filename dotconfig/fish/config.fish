be46966b1197c424a8ba4f04f8c9642a66f1a92c
Alex Bennée <alex.bennee@linaro.org>
2019-07-25 11:01:14 +0100

    config.fish: ccache and prompt glyphs

--- dotconfig/fish/config.fish
+++ dotconfig/fish/config.fish
@@ -107,11 +107,36 @@ if status --is-interactive
    add_world ~/.cargo
 end
 
+if test -d /usr/lib/ccache
+    set PATH /usr/lib/ccache $PATH
+end
+
 # TMUX setup
 if status --is-interactive
     and set -q TMUX
     set -g LP_ENABLE_TITLE
     set -g LP_ENABLE_TMUX
+
+    set -g __fish_git_prompt_show_informative_status 1
+    set -g __fish_git_prompt_hide_untrackedfiles 1
+
+    set -g __fish_git_prompt_color_branch magenta
+    set -g __fish_git_prompt_showupstream "informative"
+    set -g __fish_git_prompt_char_upstream_ahead "↑"
+    set -g __fish_git_prompt_char_upstream_behind "↓"
+    set -g __fish_git_prompt_char_upstream_prefix ""
+
+    set -g __fish_git_prompt_char_stagedstate "●"
+    set -g __fish_git_prompt_char_dirtystate "✚"
+    set -g __fish_git_prompt_char_untrackedfiles "…"
+    set -g __fish_git_prompt_char_conflictedstate "✖"
+    set -g __fish_git_prompt_char_cleanstate "✔"
+
+    set -g __fish_git_prompt_color_dirtystate blue
+    set -g __fish_git_prompt_color_stagedstate yellow
+    set -g __fish_git_prompt_color_invalidstate red
+    set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+    set -g __fish_git_prompt_color_cleanstate green
 end
 
 function ta --description "ta <session>"
