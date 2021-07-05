889ada4c74dbb1265f58d98447571a85d2b6155f
Alex Bennée <alex.bennee@linaro.org>
2021-07-05 12:12:48 +0100

    config.fish: use startship if we have it

--- dotconfig/fish/config.fish
+++ dotconfig/fish/config.fish
@@ -144,26 +144,30 @@ if status --is-interactive
     set -g LP_ENABLE_TMUX
     set -g LP_ENABLE_KENV
 
-    set -g __fish_git_prompt_show_informative_status 1
-    set -g __fish_git_prompt_hide_untrackedfiles 1
+    if type -q starship
+        starship init fish | source
+    else
+        set -g __fish_git_prompt_show_informative_status 1
+        set -g __fish_git_prompt_hide_untrackedfiles 1
 
-    set -g __fish_git_prompt_color_branch magenta
-    set -g __fish_git_prompt_showupstream "informative"
-    set -g __fish_git_prompt_char_upstream_ahead "↑"
-    set -g __fish_git_prompt_char_upstream_behind "↓"
-    set -g __fish_git_prompt_char_upstream_prefix ""
+        set -g __fish_git_prompt_color_branch magenta
+        set -g __fish_git_prompt_showupstream "informative"
+        set -g __fish_git_prompt_char_upstream_ahead "↑"
+        set -g __fish_git_prompt_char_upstream_behind "↓"
+        set -g __fish_git_prompt_char_upstream_prefix ""
 
-    set -g __fish_git_prompt_char_stagedstate "●"
-    set -g __fish_git_prompt_char_dirtystate "✚"
-    set -g __fish_git_prompt_char_untrackedfiles "…"
-    set -g __fish_git_prompt_char_conflictedstate "✖"
-    set -g __fish_git_prompt_char_cleanstate "✔"
+        set -g __fish_git_prompt_char_stagedstate "●"
+        set -g __fish_git_prompt_char_dirtystate "✚"
+        set -g __fish_git_prompt_char_untrackedfiles "…"
+        set -g __fish_git_prompt_char_conflictedstate "✖"
+        set -g __fish_git_prompt_char_cleanstate "✔"
 
-    set -g __fish_git_prompt_color_dirtystate blue
-    set -g __fish_git_prompt_color_stagedstate yellow
-    set -g __fish_git_prompt_color_invalidstate red
-    set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
-    set -g __fish_git_prompt_color_cleanstate green
+        set -g __fish_git_prompt_color_dirtystate blue
+        set -g __fish_git_prompt_color_stagedstate yellow
+        set -g __fish_git_prompt_color_invalidstate red
+        set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+        set -g __fish_git_prompt_color_cleanstate green
+    end
 end
 
 function ta --description "ta <session>"
