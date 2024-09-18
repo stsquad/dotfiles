55780f6105710f0a1bca4937b962d73f2fca42c6
Alex Bennée <alex.bennee@linaro.org>
2024-09-18 11:20:17 +0100

    fish: remove the liquid prompt stuff
    This is unmaintained and we use starfish where it counts anyway. Fish
    has since got built in fish_git_prompt support which we could fall
    back to if we care.

--- dotconfig/fish/config.fish
+++ dotconfig/fish/config.fish
@@ -160,9 +160,6 @@ if status --is-interactive
 
     # reset the default command to fish
     tmux set-option -g default-command (which fish)
-    set -g LP_ENABLE_TITLE
-    set -g LP_ENABLE_TMUX
-    set -g LP_ENABLE_KENV
 
     if type -q starship
         function _set_title -e fish_preexec --description 'Update the title'
@@ -172,27 +169,6 @@ if status --is-interactive
             end
         end
         _set_title
-    else
-        set -g __fish_git_prompt_show_informative_status 1
-        set -g __fish_git_prompt_hide_untrackedfiles 1
-
-        set -g __fish_git_prompt_color_branch magenta
-        set -g __fish_git_prompt_showupstream "informative"
-        set -g __fish_git_prompt_char_upstream_ahead "↑"
-        set -g __fish_git_prompt_char_upstream_behind "↓"
-        set -g __fish_git_prompt_char_upstream_prefix ""
-
-        set -g __fish_git_prompt_char_stagedstate "●"
-        set -g __fish_git_prompt_char_dirtystate "✚"
-        set -g __fish_git_prompt_char_untrackedfiles "…"
-        set -g __fish_git_prompt_char_conflictedstate "✖"
-        set -g __fish_git_prompt_char_cleanstate "✔"
-
-        set -g __fish_git_prompt_color_dirtystate blue
-        set -g __fish_git_prompt_color_stagedstate yellow
-        set -g __fish_git_prompt_color_invalidstate red
-        set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
-        set -g __fish_git_prompt_color_cleanstate green
     end
 end
 
